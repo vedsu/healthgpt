aa956aaf553ee8d2ac5866e14da79a5a550f14a2
vedsu <90701239+vedsu@users.noreply.github.com>
2024-09-26 14:20:11 +0530

    Update dharmendra.py

--- users/dharmendra.py
+++ users/dharmendra.py
@@ -4,10 +4,13 @@ st.session_state.user = "Dharmendra"
 import home
 
 
-tab1, tab2 = st.tabs(["📈 News", "🗃 OpenAI"])
+tab1, tab2, tab3 = st.tabs(["📰 News", "🔊 myGov", "🎰 OpenAI"])
 
 with tab1:
    home.news_data(st.session_state.user)
 
 with tab2:
+    home.gov_data(st.session_state.user)
+
+with tab3:
     home.form_data(st.session_state.user)
