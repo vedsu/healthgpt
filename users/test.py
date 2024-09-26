133066334f9aa80cab5d8399634027375c56f6aa
vedsu <90701239+vedsu@users.noreply.github.com>
2024-09-26 14:19:37 +0530

    Update test.py

--- users/test.py
+++ users/test.py
@@ -5,10 +5,13 @@ import home
 
 
 
-tab1, tab2 = st.tabs(["📈 News", "🗃 OpenAI"])
+tab1, tab2, tab3 = st.tabs(["📰 News", "🔊 myGov", "🎰 OpenAI"])
 
 with tab1:
    home.news_data(st.session_state.user)
 
 with tab2:
+    home.gov_data(st.session_state.user)
+
+with tab3:
     home.form_data(st.session_state.user)
