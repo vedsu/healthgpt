28af55b57c9fd4d2c96d18cb5b2cc0bc30ad59e3
vedsu <90701239+vedsu@users.noreply.github.com>
2024-10-17 16:40:19 +0530

    Update shubham.py

--- users/shubham.py
+++ users/shubham.py
@@ -7,13 +7,17 @@ import home
 tab1, tab2, tab3, tab4 = st.tabs(["📰 News", "🔊 myGov", "🚀federal", "🎰 OpenAI"])
 
 with tab1:
+   
    home.news_data(st.session_state.user)
 
 with tab2:
+   
     home.gov_data(st.session_state.user)
 
 with tab3:
+   
     home.fed_gov(st.session_state.user)
 
 with tab4:
+   
     home.form_data(st.session_state.user)
